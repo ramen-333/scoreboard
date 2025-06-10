4716187bb5bf91281dbbd65b96029726e4682419
ramen-333 <127578842+ramen-333@users.noreply.github.com>
2025-06-10 13:20:38 -0700

    Commit initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+let home_score_el = document.getElementById("home-score")
+home_score = 0
+let guest_score_el = document.getElementById("guest-score")
+guest_score = 0
+
+function home_plus_one() {
+    home_score += 1
+    home_score_el.textContent = home_score
+}
+
+function home_plus_two() {
+    home_score += 2
+    home_score_el.textContent = home_score
+}
+
+function home_plus_three() {
+    home_score += 3
+    home_score_el.textContent = home_score
+}
+
+function guest_plus_one() {
+    guest_score += 1
+    guest_score_el.textContent = guest_score
+}
+
+function guest_plus_two() {
+    guest_score += 2
+    guest_score_el.textContent = guest_score
+}
+
+function guest_plus_three() {
+    guest_score += 3
+    guest_score_el.textContent = guest_score
+}
